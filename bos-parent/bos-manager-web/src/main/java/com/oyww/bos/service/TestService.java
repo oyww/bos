a038c3b231dccdde36c6d2b845efb341ab449b7d
oyww <289002360@qq.com>
2020-12-19 14:35:24 +0800

    完成web模块连接注册中心

--- bos-parent/bos-manager-web/src/main/java/com/oyww/bos/service/TestService.java
+++
@@ -1,5 +0,0 @@
-package com.oyww.bos.service;
-
-public interface TestService {
-    String say(String msg);
-}
--- bos-parent/bos-manager-web/src/main/java/com/oyww/bos/web/action/HellloAction.java
+++ bos-parent/bos-manager-web/src/main/java/com/oyww/bos/web/action/HellloAction.java
@@ -2,13 +2,17 @@ package com.oyww.bos.web.action;
 
 import com.opensymphony.xwork2.Action;
 import com.opensymphony.xwork2.ActionSupport;
+import com.oyww.bos.service.TestService;
 import org.apache.struts2.ServletActionContext;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class HellloAction extends ActionSupport {
    private String name;
+   @Autowired
+   private TestService testService;
 
 
     public void setName(String name) {
@@ -17,10 +21,11 @@ public class HellloAction extends ActionSupport {
 
     public String execute() throws Exception {
         System.out.println(name);
+        String say = testService.say(name);
         HttpServletResponse response = ServletActionContext.getResponse();
         response.setHeader("Content-Type","text/html;charset=UTF-8");
         HttpServletRequest request = ServletActionContext.getRequest();
-        request.setAttribute("name",name);
+        request.setAttribute("name",say);
         return "success";
     }
 }
